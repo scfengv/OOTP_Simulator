d383b2b0c42af144879374b92fa0413d20608fa0
scfengv <scvfeng27@gmail.com>
2024-07-15 14:06:22 +0800

    Update 0715

--- ootp_file.py
+++ ootp_file.py
@@ -107,7 +107,7 @@ def ootp_file():
         file_operator.check_game()
         file_operator.move_game()
 
-    file_operator.remove_lg_folders()
+    # file_operator.remove_lg_folders()
     
 if __name__ == "__main__":
     ootp_file()
